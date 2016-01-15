8c7333dfc54188883314c42758c03ac01c964825
Tedward Erker <erker@wisc.edu>
2016-01-15 14:03:40 -0600

    added assert_almost_equal test

--- tests_stats.py
+++ tests_stats.py
@@ -1,5 +1,5 @@
 from stats import mean
-from nose.tools import assert_equal
+from nose.tools import assert_equal, assert_almost_equals
 
 
 def test_mean():
@@ -18,5 +18,5 @@ def test_mean_float():
 
 
 def test_mean_float2():
-    assert_equal(mean([.5,.5,1]), .666)
+    assert_almost_equals(mean([.5,.5,1]), .666666666)
 
