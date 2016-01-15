981af3a50db40af6bcd99e33f761a236b12eb4e0
Tedward Erker <erker@wisc.edu>
2016-01-15 13:35:04 -0600

    added test of mean, simple case

--- stats.py
+++ stats.py
@@ -1,7 +1,20 @@
 def mean(vals):
     ''' calculate arithmetic mean of list'''
+    assert type(vals) is list, "wrong input format"
     total = sum(vals)
     length = len(vals)
     return total/length
 
-print(mean([2,4])
+
+# print(mean([2,4]))
+
+def test_mean():
+      assert mean([2,4]) == 3.0
+
+test_mean()
+
+# def test_empty_list():
+#     assert mean([]) == 0.0
+
+# test_empty_list()
+
